982a30137e7fbb6139061ded352f4183455bdf5a
Matsushin <matsu1983@gmail.com>
2020-03-01 15:40:41 +0900

    rubyメソッドの課題の回答

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -11,52 +11,54 @@ class Prefecture
     def names
       # 「map」メソッドを使って都道府県名の配列を作ろう
       # PREFECTURES.map ...
-
+      PREFECTURES.map { |pref| pref[:name] }
     end
 
     def counts
       # 「pluck」メソッドを使って人口の配列を作ろう
-
+      PREFECTURES.pluck(:count)
     end
 
     def unique_area_cd
       # 「uniq」メソッドを使って重複しないarea_cdの配列を作ろう
-
+      PREFECTURES.pluck(:area_cd).uniq
     end
 
     def total_count
       # 「inject」メソッドを使って各都道府県のcountを集計してみよう
-
+      PREFECTURES.inject(0) { |sum, pref| sum + pref[:count] }
     end
 
     def join_names
       # 「join」メソッドを使って各都道府県の名称(name)をカンマ区切りで繋げてみよう
-
+      names.join(',')
     end
 
     def find_by_count(count)
       # 「find」メソッドを使って人口数(count)が引数のcountより小さい最初の県名を取得しよう
-
+      PREFECTURES.find { |pref| pref[:count] < count }[:name]
     end
 
     def select_by_count(count)
       # 「select」メソッドを使って人口数(count)が引数のcountより大きい県名を取得しよう
-
+      PREFECTURES.select do |pref|
+        pref[:count] > count
+      end.pluck(:name)
     end
 
     def group_by_area
       # 「group_by」メソッドを使って「area_cd」でグループ分けしてみよう
-
+      PREFECTURES.group_by { |pref| pref[:area_cd] }
     end
 
     def name_counts
       # 「to_h」メソッドを使って都道府県名(name)をキー、countがバリューのHashを作ろう
-
+      PREFECTURES.pluck(:name, :count).to_h
     end
 
     def add_prefecture(prefecture)
       # 「push」メソッドを使って都道府県名を情報を追加して新しいHashを作ろう
-
+      PREFECTURES.push(prefecture)
     end
   end
 end
